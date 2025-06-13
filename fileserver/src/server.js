96fbfa682595cafaa443b2303ae922b6ff04fe59
haiba <9700samarth@gmail.com>
2025-06-13 12:44:05 +0530

    fileserver update

--- fileserver/src/server.js
+++ fileserver/src/server.js
@@ -1,22 +1,34 @@
 const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3001;
 
-const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads/');
+    const empId =  req.body.employeeId;
+    
+    const empDir = path.join(__dirname, '..', 'uploads', empId);
+
+    fs.mkdir(empDir, {recursive: true}, (err) => {
+      if(err) return cb(err);
+      cb(null, empDir);
+    })
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + ext);
   }
 })
 
 const upload = multer({ storage: storage })
 
-app.post('/api/upload/', upload.single('file'), (req, res) => {
-  res.send('bich');
+app.post('/api/upload/', upload.single('adhaar'), (req, res) => {
+  res.json({
+    path: req.file.path
+  });
 })
 
 app.listen(port, () => {
